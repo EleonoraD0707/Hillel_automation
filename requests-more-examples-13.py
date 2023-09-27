d4b2dc6ee6587ac4983e6a44bf6bceec65cd6fe7
EleonoraD0707 <116564387+EleonoraD0707@users.noreply.github.com>
2023-09-27 10:05:19 +0200

    Add files via upload

---
+++ requests-more-examples-13.py
@@ -0,0 +1,32 @@
+import requests
+
+class UserSignUpModel:
+
+    def __init__(self, name, last_name, email, password, repeat_password):
+        self.name = name
+        self.lastName = last_name
+        self.email = email
+        self.password = password
+        self.repeatPassword = repeat_password
+
+class TestRegistration:
+
+    def setup_class(self):
+        self.user_to_sign_up = UserSignUpModel("Jane", "Dawn", "nafnafnaf123@test.com", "Qwerty12345", "Qwerty12345")
+
+
+    def setup_method(self):
+        self.session = requests.session()
+
+    def test_check_successful_user_registration(self):
+        post_new_user = self.session.post(url="https://qauto2.forstudy.space/api/auth/signup", json=self.user_to_sign_up.__dict__)
+        assert post_new_user.status_code == 201
+
+
+    def test_check_successful_user_registration(self):
+        post_new_user = self.session.post(url="https://qauto2.forstudy.space/api/auth/signup", json=self.user_to_sign_up.__dict__)
+        assert post_new_user.status_code == 201
+
+
+    def teardown_method(self):
+        self.session.delete("https://qauto2.forstudy.space/api/users")
